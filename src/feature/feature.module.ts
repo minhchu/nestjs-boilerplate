a6434eb25a8630ec12b0253b46cb9b3c05d04d29
Minh Chu <scsilver94@gmail.com>
2023-08-10 01:28:41 +0700

    Remove nestjs cache-manager

--- src/feature/feature.module.ts
+++ src/feature/feature.module.ts
@@ -1,10 +1,8 @@
-import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { FeatureController } from './feature.controller';
 import { FeatureService } from './feature.service';
 
 @Module({
-  // imports: [CacheModule.register()],
   controllers: [FeatureController],
   providers: [FeatureService],
 })
